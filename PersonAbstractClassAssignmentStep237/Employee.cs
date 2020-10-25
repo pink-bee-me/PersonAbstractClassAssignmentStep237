ef9a257722aab404b9a8e9795e686f9530adc5cc
LaurieSue <pink.bee.me@outlook.com>
2020-10-25 00:18:48 -0500

    Add project files.

---
+++ PersonAbstractClassAssignmentStep237/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonAbstractClassAssignmentStep237
+{
+   public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("You have added a new Person Object to the Employee Class.");
+            base.SayName();
+            
+        }
+    }
+}
---
+++ PersonAbstractClassAssignmentStep237/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonAbstractClassAssignmentStep237
+{
+   public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual void SayName()
+        {
+            string fullName = FirstName + " " + LastName;
+            Console.WriteLine("Name of Person Added To System: " + fullName);
+        }
+    }
+}
